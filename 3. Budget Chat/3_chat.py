3d2cbb0eebfc40f53b29777b4f7f7dea7ec53343
Ryan Gang <ryan-gg@outlook.com>
2023-10-20 23:51:25 +0530

    chat-server v3 Passes all tests.

--- 3. Budget Chat/3_chat.py
+++ 3. Budget Chat/3_chat.py
@@ -2,8 +2,14 @@ import logging
 import socket
 import sys
 import threading
-from typing import Optional
-from helpers import valid_name
+
+from helpers import (
+    broadcast_to_all_except,
+    process_exiting_client,
+    process_new_client,
+    send_and_log,
+    valid_name,
+)
 
 logging.basicConfig(
     format=(
@@ -16,80 +22,40 @@ logging.basicConfig(
 )
 
 IP, PORT = "10.138.0.2", 9090
-CLIENTS: dict[str, socket.socket] = {}
-
-
-def broadcast_room_details(client: str) -> None:
-    conn = CLIENTS[client]
-    names = list(CLIENTS.keys())
-    participants = ", ".join(list(filter(lambda x: x != client, names)))
-    message = f"* The room contains: {participants}\n"
-    send_and_log(conn, message, client)
-
-
-def broadcast_to_all_except(text: str, excluded_client: Optional[str], chat_mode=False) -> None:
-    for name in CLIENTS:
-        if name != excluded_client:
-            conn = CLIENTS[name]
-            if chat_mode:
-                response = f"[{excluded_client}] {text}\n"
-            else:
-                response = text
-            send_and_log(conn, response, name)
-
-
-def process_new_client(name: str, conn: socket.socket):
-    CLIENTS[name] = conn
-    client_name = name
-    joining_message = f"* {name} has entered the room\n"
-    broadcast_to_all_except(joining_message, client_name)
-
-    # if len(CLIENTS.keys()) > 1:
-    broadcast_room_details(client_name)
-
-
-def process_exiting_client(name: Optional[str], conn: socket.socket, addr):
-    logging.debug(f"Client disconnected : {addr}")
-    if name in CLIENTS:
-        del CLIENTS[name]
-    conn.close()
-    exiting_message = f"* {name} has left the room\n"
-    broadcast_to_all_except(exiting_message, name)
-
-
-def send_and_log(conn: socket.socket, response: str, client_name: str):
-    conn.send(response.encode())
-    logging.info(f"Response : {response}")
-    logging.info(f"Sent {len(response)} bytes to {client_name}")
+DEFAULT_CLIENT_NAME = ""
 
 
 def handler(conn: socket.socket, addr: socket.AddressFamily):
     # Handler serves every connection, until it's closed.
-    client_name: Optional[str] = None
-    size = 1024
+    client_name: str = DEFAULT_CLIENT_NAME
+    request = ""
+    size = 2
 
     logging.info(f"Connected by : {addr}")
+    response = "Welcome to budgetchat! What shall I call you?\n"
+    send_and_log(conn, response, "new_client")
+
     while True:
-        if not client_name:
-            response = "Welcome to budgetchat! What shall I call you?\n"
-            send_and_log(conn, response, "new_client")
         message = conn.recv(size)
-        logging.debug(message)
-        if not message.strip():
+        request += message.decode()
+        if not message:
             # Client disconnected
             process_exiting_client(client_name, conn, addr)
             break
-
-        if not client_name:
-            name = message.decode().strip()
-            if valid_name(name):
-                process_new_client(name, conn)
-                client_name = name
+        if request.endswith("\n"):
+            request = request.strip()
+            logging.debug(f"Request : {request}")
+            if client_name == DEFAULT_CLIENT_NAME:
+                name = request
+                if valid_name(name):
+                    process_new_client(name, conn)
+                    client_name = name
+                else:
+                    process_exiting_client(client_name, conn, addr)
+                    break
             else:
-                process_exiting_client(client_name, conn, addr)
-        else:
-            broadcast_to_all_except(message.decode(), client_name, chat_mode=True)
-        logging.debug(f"Request : {message}")
+                broadcast_to_all_except(request, client_name, chat_mode=True)
+            request = ""
 
 
 def main():
@@ -97,8 +63,7 @@ def main():
     logging.info(f"Started Server @ {IP}:{PORT}")
     while True:
         conn, addr = server_socket.accept()
-        conn.settimeout(60)  # Set timeout for connection to 10 seconds.
-        # Spin out a new thread to process this request, return control back to main thread.
+        conn.settimeout(60)  # Set timeout for connection to 60 seconds.
         threading.Thread(target=handler, args=(conn, addr)).start()
 
 
--- 3. Budget Chat/helpers.py
+++ 3. Budget Chat/helpers.py
@@ -1,6 +1,65 @@
+import logging
 import re
+import socket
+
+DEFAULT_CLIENT_NAME = ""
+CLIENTS: dict[str, socket.socket] = {}  # Name -> socket connection mapping for all clients.
+# clients_lock = threading.RLock()  # Not really needed.
 
 
 def valid_name(name: str) -> bool:
     match = re.fullmatch("[A-Za-z0-9]{1,}", name)
     return match is not None
+
+
+def broadcast_room_details(client: str) -> None:
+    # with clients_lock:
+    conn = CLIENTS[client]
+    participants = ", ".join(list(filter(lambda x: x != client, list(CLIENTS.keys()))))
+    message = f"* The room contains: {participants}\n"
+    send_and_log(conn, message, client)
+
+
+def broadcast_to_all_except(text: str, excluded_client: str, chat_mode=False) -> None:
+    # with clients_lock:
+    logging.debug(f"Sending broadcast to {list(CLIENTS.keys())} except {excluded_client}")
+    try:
+        for name in CLIENTS:
+            if name != excluded_client:
+                conn = CLIENTS[name]
+                if chat_mode:
+                    response = f"[{excluded_client}] {text}\n"
+                else:
+                    response = text
+                send_and_log(conn, response, name)
+    except RuntimeError as E:
+        logging.error(E)
+
+
+def process_new_client(name: str, conn: socket.socket):
+    # with clients_lock:
+    CLIENTS[name] = conn
+    joining_message = f"* {name} has entered the room\n"
+    broadcast_to_all_except(joining_message, name)
+    # if len(CLIENTS.keys()) > 1:
+    broadcast_room_details(name)
+
+
+def process_exiting_client(name: str, conn: socket.socket, addr, broadcast: bool = True):
+    logging.debug(f"Client : {name} @ {addr} disconnected.")
+    # with clients_lock:
+    conn.close()
+    if name != DEFAULT_CLIENT_NAME:
+        if name in CLIENTS:
+            CLIENTS.pop(name)
+        exiting_message = f"* {name} has left the room\n"
+        broadcast_to_all_except(exiting_message, name)
+
+
+def send_and_log(conn: socket.socket, response: str, client_name: str):
+    try:
+        conn.send(response.encode())
+        logging.info(f"Response : {response.strip()}")
+        logging.info(f"Sent {len(response)} bytes to {client_name}")
+    except Exception as E:
+        logging.error(E)
