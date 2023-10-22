c6044fa9d7f6a87cc05d3213e379f9eab18b16d6
Ryan Gang <ryan-gg@outlook.com>
2023-10-22 20:36:10 +0530

    mitm-server v2. MITM server with address rewrite. Passes some tests.

--- 5. Mob in the Middle/5_mitm.py
+++ 5. Mob in the Middle/5_mitm.py
@@ -2,7 +2,7 @@ import logging
 import socket
 import sys
 import threading
-from helpers import recv_data, send_data
+from helpers import recv_data, send_data, open_upstream_conn, rewrite_address
 
 
 logging.basicConfig(
@@ -16,46 +16,35 @@ logging.basicConfig(
 )
 
 IP, PORT = "10.138.0.2", 9090
-UPSTREAM_IP, UPSTREAM_PORT = "chat.protohackers.com", 16963
 
 
-def handler(c_conn: socket.socket, c_addr: socket.AddressFamily):
-    """
-    c_conn and c_addr correspond to conn and addr for client.
-    UPSTREAM_SERVER   <-- -->   MITM_PROXY   <-- -->   CLIENT
+def handler(
+    up_conn: socket.socket,
+    up_addr: socket.AddressFamily,
+    down_conn: socket.socket,
+    down_addr: socket.AddressFamily,
+):
+    logging.info(f"Connected to downstream @ {down_addr} and upstream @ {up_addr}")
 
-    1. Open connection with upstream.
-
-    2. Wait for recv() from upstream.
-    3. Send data to client.
-    4. Wait for recv() from client.
-    5. Send data to upstream.
-    6. GoTo 2.
-    """
-    logging.info(f"Connected by client : {c_addr}")
-    # Open connection with upstream.
-    upstream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4, TCP
-    upstream.connect((UPSTREAM_IP, UPSTREAM_PORT))
-    logging.info(f"Connected to upstream : {UPSTREAM_IP}@{UPSTREAM_PORT}")
-
-    flag = True
     while 1:
-        if flag:
-            data = recv_data(upstream)
-            send_data(c_conn, data)
-        else:
-            data = recv_data(c_conn)
-            send_data(upstream, data)
-        flag ^= True
+        data = recv_data(up_conn)
+        if not data:
+            up_conn.close()
+            down_conn.close()
+            return
+        data = rewrite_address(data)
+        send_data(down_conn, data)
 
 
 def main():
     server_socket = socket.create_server((IP, PORT), reuse_port=True)
-    logging.info(f"Started Server @ {IP}:{PORT}")
+    logging.info(f"Started MITM Server @ {IP}:{PORT}")
     while True:
         conn, addr = server_socket.accept()
         conn.settimeout(60)  # Set timeout for connection to 60 seconds.
-        threading.Thread(target=handler, args=(conn, addr)).start()
+        upstream, upstream_addr = open_upstream_conn()
+        threading.Thread(target=handler, args=(upstream, upstream_addr, conn, addr)).start()
+        threading.Thread(target=handler, args=(conn, addr, upstream, upstream_addr)).start()
 
 
 if __name__ == "__main__":
--- 5. Mob in the Middle/helpers.py
+++ 5. Mob in the Middle/helpers.py
@@ -1,5 +1,8 @@
 import logging
+import re
 import socket
+
+UPSTREAM_IP, UPSTREAM_PORT = "chat.protohackers.com", 16963
 
 
 def recv_data(conn: socket.socket):
@@ -23,3 +26,23 @@ def send_data(conn: socket.socket, response: str):
         # logging.info(f"Sent {len(response)} bytes.")
     except Exception as E:
         logging.error(E)
+
+
+def open_upstream_conn() -> tuple[socket.socket, str]:
+    # Open connection with upstream.
+    upstream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPv4, TCP
+    upstream.connect((UPSTREAM_IP, UPSTREAM_PORT))
+    upstream_addr = f"{UPSTREAM_IP}:{UPSTREAM_PORT}"
+    logging.info(f"Connected to upstream : {upstream_addr}")
+    return upstream, upstream_addr
+
+
+def rewrite_address(data: str) -> str:
+    ADDRESS = "7YWHMfk9JZe0LM0g1ZauHuiSxhI"
+
+    pattern = "7[A-Za-z0-9]{25,34}"
+    matches = re.findall(pattern, data)
+    for match in matches:
+        data = data.replace(match, ADDRESS)
+
+    return data
