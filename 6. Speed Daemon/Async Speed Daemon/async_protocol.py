73ec41945efe9b6509f217026cd593811b36c74d
Ryan Gang <ryan-gg@outlook.com>
2023-11-02 16:37:42 +0530

    Add Async Serializer for Speed Cam.

--- 6. Speed Daemon/Async Speed Daemon/async_protocol.py
+++ 6. Speed Daemon/Async Speed Daemon/async_protocol.py
@@ -1,7 +1,7 @@
 import logging
 import struct
 import sys
-from asyncio import StreamReader
+from asyncio import StreamReader, StreamWriter
 from typing import Callable
 
 logging.basicConfig(
@@ -14,29 +14,27 @@ logging.basicConfig(
     handlers=[logging.FileHandler("app.log"), logging.StreamHandler(sys.stdout)],
 )
 
+# Messages types -> Integer message codes. (Can be serialized to their exp values)
+MSG_CODES = {
+    "ERROR": 16,  # 0x10
+    "PLATE": 32,  # 0x20
+    "TICKET": 33,  # 0x21
+    "WHEARTBEAT": 64,  # 0x40
+    "HEARTBEAT": 65,  # 0x41
+    "CAMERA": 128,  # 0x80
+    "DISPATCHER": 129,  # 0x81
+}
+# Format strings.
+U8 = ">B"
+U16 = ">H"
+U32 = ">I"
+LP_STR: Callable[[int], str] = lambda length: ">" + "B" * length  # Length prefixed str
+
 
 class Parser(object):
-    def __init__(self):
-        self.u8 = ">B"
-        self.u16 = ">H"
-        self.u32 = ">I"
-        self.lp_str: Callable[[int], str] = lambda length: ">" + "B" * length
-        # Length prefixed str
-
-        # Int codes of message types against messages types.
-        self.codes = {
-            "ERROR": 16,  # 0x10
-            "PLATE": 32,  # 0x20
-            "TICKET": 33,  # 0x21
-            "WHEARTBEAT": 64,  # 0x40
-            "HEARTBEAT": 65,  # 0x41
-            "CAMERA": 128,  # 0x80
-            "DISPATCHER": 129,  # 0x81
-        }
-
     async def parse_str(self, reader: StreamReader) -> str:
         length = await self.parse_uint8(reader)
-        data = await self.parse_array_uint(reader, length, self.u8, bits=8)
+        data = await self.parse_array_uint(reader, length, U8, bits=8)
         return "".join(chr(ascii) for ascii in data)
 
     async def _parse_uint(self, reader: StreamReader, fmt: str, bits: int) -> int:
@@ -45,13 +43,13 @@ class Parser(object):
         return unpacked
 
     async def parse_uint8(self, reader: StreamReader) -> int:
-        return await self._parse_uint(reader, fmt=self.u8, bits=8)
+        return await self._parse_uint(reader, fmt=U8, bits=8)
 
     async def parse_uint16(self, reader: StreamReader) -> int:
-        return await self._parse_uint(reader, fmt=self.u16, bits=16)
+        return await self._parse_uint(reader, fmt=U16, bits=16)
 
     async def parse_uint32(self, reader: StreamReader) -> int:
-        return await self._parse_uint(reader, fmt=self.u32, bits=32)
+        return await self._parse_uint(reader, fmt=U32, bits=32)
 
     async def parse_array_uint(
         self, reader: StreamReader, array_length: int, fmt: str, bits: int
@@ -91,6 +89,107 @@ class Parser(object):
     # Type : 81
     async def parse_iamdispatcher_data(self, reader: StreamReader) -> list[int]:
         num_roads = await self.parse_uint8(reader)
-        roads = await self.parse_array_uint(reader, num_roads, self.u16, 16)
+        roads = await self.parse_array_uint(reader, num_roads, U16, 16)
 
         return roads
+
+
+class Serializer(object):
+    async def _serialize_lp_str(self, data: str) -> bytes:
+        return await self._serialize_uint8(len(data)) + await self._serialize_str(data)
+
+    async def _serialize_str(self, data: str) -> bytes:
+        return bytes(data, "utf-8")
+
+    async def _serialize_uint(self, str_bytes: int, fmt: str) -> bytes:
+        return struct.pack(fmt, str_bytes)
+
+    async def _serialize_uint8(self, str_bytes: int) -> bytes:
+        return await self._serialize_uint(str_bytes, U8)
+
+    async def _serialize_uint16(self, str_bytes: int) -> bytes:
+        return await self._serialize_uint(str_bytes, U16)
+
+    async def _serialize_uint32(self, str_bytes: int) -> bytes:
+        return await self._serialize_uint(str_bytes, U32)
+
+    async def serialize_error_data(self, msg: str) -> bytes:
+        CODE_NAME = "ERROR"
+        CODE = MSG_CODES[CODE_NAME]
+
+        code = await self._serialize_uint8(CODE)
+        data = await self._serialize_lp_str(msg)
+        return code + data
+
+    async def serialize_ticket_data(
+        self,
+        plate: str,
+        road: int,
+        mile1: int,
+        timestamp1: int,
+        mile2: int,
+        timestamp2: int,
+        speed: int,
+    ) -> bytes:
+        CODE_NAME = "TICKET"
+        CODE = MSG_CODES[CODE_NAME]
+
+        data = bytearray()
+        data.extend(await self._serialize_uint8(CODE))
+        data.extend(await self._serialize_lp_str(plate))
+        data.extend(await self._serialize_uint16(road))
+        data.extend(await self._serialize_uint16(mile1))
+        data.extend(await self._serialize_uint32(timestamp1))
+        data.extend(await self._serialize_uint16(mile2))
+        data.extend(await self._serialize_uint32(timestamp2))
+        data.extend(await self._serialize_uint16(speed))
+
+        return bytes(data)
+
+    async def serialize_heartbeat_data(self) -> bytes:
+        CODE_NAME = "HEARTBEAT"
+        CODE = MSG_CODES[CODE_NAME]
+
+        data = await self._serialize_uint8(CODE)
+        return data
+
+
+class SocketHandler(object):
+    def __init__(self, reader: StreamReader, writer: StreamWriter) -> None:
+        self.reader = reader
+        self.writer = writer
+        self.p = Parser()
+
+    async def write(self, data: str):
+        if not data.endswith("\n"):
+            data += "\n"
+        self.writer.write(data.encode())
+        logging.debug(f"Response : {data.strip()}")
+        logging.debug(f"Sent {len(data)} bytes.")
+        return await self.writer.drain()
+
+    async def close(self, error_msg: str, conn: str):
+        await self.write(error_msg)
+        self.writer.write_eof()
+        self.writer.close()
+        logging.info(f"Closed connection to client @ {conn}.")
+
+    async def read(self):
+        try:
+            msg_code = await self.p.parse_message_type(self.reader)
+        except ConnectionRefusedError:
+            logging.error("Client Disconnected.")
+            return ""
+        if msg_code == 20:
+            _ = await self.p.parse_plate_data(self.reader)
+        elif msg_code == 40:
+            _ = await self.p.parse_wantheartbeat_data(self.reader)
+        elif msg_code == 80:
+            _ = await self.p.parse_iamcamera_data(self.reader)
+        elif msg_code == 81:
+            _ = await self.p.parse_iamdispatcher_data(self.reader)
+        else:
+            raise RuntimeError("Unexpected msg_type")
+
+        logging.debug(f"Type : {msg_code}, Data : {_}")
+        return msg_code, _
